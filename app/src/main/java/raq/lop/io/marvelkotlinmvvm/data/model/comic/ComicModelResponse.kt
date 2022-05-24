5cbf2ea64899d61e037f28399f67ebba14344c3a
Raquel Maria <raquel.silva1@outlook.com.br>
2022-05-24 20:58:03 -0300

    added comics data classes

---
+++ app/src/main/java/raq/lop/io/marvelkotlinmvvm/data/model/comic/ComicModelResponse.kt
@@ -0,0 +1,9 @@
+package raq.lop.io.marvelkotlinmvvm.data.model.comic
+
+import com.google.gson.annotations.SerializedName
+import java.io.Serializable
+
+data class ComicModelResponse(
+    @SerializedName("data")
+    val data: ComicModelData
+): Serializable
